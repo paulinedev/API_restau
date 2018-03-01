21dbde64f762eaa16c7e552a02c35727f5079ce1
paulinedev <pauline.de.vallois@gmail.com>
2018-03-01 09:39:24 +0100

    04

--- app.js
+++ app.js
@@ -39,3 +39,22 @@ var menu = {
   },
 };
 console.log(menu.dessert.dessert1);
+
+//MAP
+
+var map = L.map('map').setView([48.866667, 2.333333], 13);
+
+L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+L.marker([48.866667, 2.333333]).addTo(map)
+    .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+    .openPopup();
+
+    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+  attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+  maxZoom: 18,
+  id: 'mapbox.streets',
+  accessToken: 'your.mapbox.access.token'
+}).addTo(mymap);
