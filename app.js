e5e3d2ca11788e29243cc73d38a5ca25adc65cfa
paulinedev <pauline.de.vallois@gmail.com>
2018-03-07 09:48:48 +0100

    géoloc

--- app.js
+++ app.js
@@ -40,21 +40,46 @@ var menu = {
 };
 console.log(menu.dessert.dessert1);
 
-//MAP
+//MAP Geolocalisé
 
-var map = L.map('map').setView([48.866667, 2.333333], 13);
+ var map = L.map('map');
 
-L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+     maxZoom: 19,
+     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+   maxZoom: 18
+ }).addTo(map);
 
-L.marker([48.866667, 2.333333]).addTo(map)
-    .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-    .openPopup();
+   map.locate({setView: true, maxZoom: 16});
+   function onLocationFound(e) {
+   var radius = e.accuracy / 2;
 
-    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-  attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
-  maxZoom: 18,
-  id: 'mapbox.streets',
-  accessToken: 'your.mapbox.access.token'
-}).addTo(mymap);
+   L.marker(e.latlng).addTo(map)
+       .bindPopup("Vous êtes à au moins " + radius + " mètres de ce point").openPopup();
+
+   L.circle(e.latlng, radius).addTo(map);
+}
+
+map.on('locationfound', onLocationFound);
+function onLocationError(e) {
+   alert(e.message);
+}
+
+map.on('locationerror', onLocationError);
+
+// var map = L.map('map').setView([48.866667, 2.333333], 13);
+//
+// L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+//     attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+// }).addTo(map);
+//
+// L.marker([48.866667, 2.333333]).addTo(map)
+//     .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+//     .openPopup();
+//
+//     L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+//   attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+//   maxZoom: 18,
+//   id: 'mapbox.streets',
+//   accessToken: 'your.mapbox.access.token'
+// }).addTo(mymap);
